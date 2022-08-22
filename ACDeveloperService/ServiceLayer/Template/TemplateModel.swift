7accb20c92f7c7c791bc60164d061c1454cc53e8
Dmitriy Polyakov <dmitriyap11@gmail.com>
2022-08-22 22:40:32 +0300

    Add fetch methods

--- ACDeveloperService/ServiceLayer/Template/TemplateModel.swift
+++ ACDeveloperService/ServiceLayer/Template/TemplateModel.swift
@@ -8,11 +8,34 @@
 import Foundation
 
 struct TemplateModel {
+    
+    // MARK: - Init
+    init(id: String, name: String, fileName: String, fileExtension: String) {
+        self.id = id
+        self.name = name
+        self.fileName = fileName
+        self.fileExtension = fileExtension
+    }
+    
+    init(url: URL) {
+        let lastPathComponent = url.lastPathComponent as NSString
+        let name = String(lastPathComponent.deletingPathExtension)
+        let fileName = url.lastPathComponent
+        let fileExtension = String(lastPathComponent.pathExtension)
+        
+        self.init(
+            id: name,
+            name: name,
+            fileName: fileName,
+            fileExtension: fileExtension
+        )
+    }
+    
+    // MARK: - Props
     let id: String
     let name: String
     let fileName: String
     let fileExtension: String
-    let fileURL: URL
 }
 
 // MARK: - Identifiable
--- ACDeveloperService/ServiceLayer/Template/TemplateService.swift
+++ ACDeveloperService/ServiceLayer/Template/TemplateService.swift
@@ -13,77 +13,92 @@ class TemplateService {
     private let fileManager: FileManager = .default
     
     // MARK: - Methods
-    func downloadTemplatesFromGITHUB() {
-        guard let url = URL(string: "https://github.com/AppCraftTeam/appcraft-developer-service-macos/archive/refs/heads/main.zip") else { return }
+    func fetchTemplatesFromBundle(completion: ContextClosure<Error?>? = nil) {
+        do {
+            let folderSaveUrl = try self.getOrCreateTemplatesFolderURL()
+            let bundleUrls = Bundle.main.urls(forResourcesWithExtension: "zip", subdirectory: nil) ?? []
+            
+            for bundleUrl in bundleUrls {
+                let template = TemplateModel(url: bundleUrl)
+                guard let templateUrl = Bundle.main.url(forResource: template.name, withExtension: template.fileExtension) else { continue }
+                let templateSaveUrl = folderSaveUrl.appendingPathComponent(template.fileName)
+                print("!!! bundleUrl:", bundleUrl)
+                print("!!! templateSaveUrl:", templateSaveUrl)
+                try self.fileManager.removeIfExists(at: templateSaveUrl)
+                try self.fileManager.copyItem(at: templateUrl, to: templateSaveUrl)
+            }
+            
+            completion?(nil)
+        } catch {
+            completion?(error)
+        }
+    }
+    
+    func fetchTemplatesFromGITHUB() {
+        let branchName = "main"
+        let fileExtension = "zip"
+        let fileName = "\(branchName).\(fileExtension)"
+        guard let url = URL(string: "https://github.com/AppCraftTeam/appcraft-developer-service-macos/archive/refs/heads/\(fileName)") else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
+
         
-        let task = URLSession.shared.downloadTask(with: request) { url, response, error in
+        let task = URLSession.shared.downloadTask(with: request) { [weak self] url, response, error in
+            guard let self = self else { return }
             guard let fileUrl = url else { return }
             
+            print("!!! response error", error)
+            
             do {
-                let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-                let savedURL = documentsURL.appendingPathComponent(fileUrl.lastPathComponent)
-                try FileManager.default.moveItem(at: fileURL, to: savedURL)
+                let documentsURL = try self.fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                let archiveURL = documentsURL.appendingPathComponent(fileName)
+                let unzipURL = documentsURL.appendingPathComponent(branchName)
                 
-                print("!!! savedURL", savedURL)
+                try self.fileManager.clearDocumentDirectory()
+                
+                try self.fileManager.moveItem(at: fileUrl, to: archiveURL)
+                try self.fileManager.unzipItem(at: archiveURL, to: unzipURL)
+                try self.fileManager.removeIfExists(at: archiveURL)
+                
+                guard let applicationSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else { return }
+                
+                let paths = self.fileManager.enumerator(atPath: unzipURL.path)?.allObjects as? [String] ?? []
+                let templatesPaths = paths.filter({ $0.contains(".zip") })
+                
+                for path in templatesPaths {
+                    let fullUrl = unzipURL.appendingPathComponent(path)
+                    let saveTemplateUrl = applicationSupportURL.appendingPathComponent(fullUrl.lastPathComponent)
+                    
+                    try self.fileManager.removeIfExists(at: saveTemplateUrl)
+                    try self.fileManager.moveItem(at: fullUrl, to: saveTemplateUrl)
+                }
+                
+                try self.fileManager.clearDocumentDirectory()
+            
             } catch {
                 print("!!! error", error)
             }
             
         }
-//        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-//                let session = NSURLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-//                let request = NSMutableURLRequest(URL: URL)
-//                request.HTTPMethod = "GET"
-//                let task = session.dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
-//                    if (error == nil) {
-//                        // Success
-//                        let statusCode = (response as NSHTTPURLResponse).statusCode
-//                        println("Success: \(statusCode)")
-//
-//                        // This is your file-variable:
-//                        // data
-//                    }
-//                    else {
-//                        // Failure
-//                        println("Failure: %@", error.localizedDescription);
-//                    }
-//                })
-//                task.resume()
+        
+        task.resume()
     }
     
     func getTemplates(completion: ContextClosure<[TemplateModel]>){
         let urls = Bundle.main.urls(forResourcesWithExtension: "zip", subdirectory: nil) ?? []
-        
-        let result: [TemplateModel] = urls.map { url in
-            let lastPathComponent = url.lastPathComponent as NSString
-            let name = String(lastPathComponent.deletingPathExtension)
-            let fileName = url.lastPathComponent
-            let fileExtension = String(lastPathComponent.pathExtension)
-            
-            return .init(
-                id: name,
-                name: name,
-                fileName: fileName,
-                fileExtension: fileExtension,
-                fileURL: url
-            )
-        }
-        
+        let result: [TemplateModel] = urls.map { .init(url: $0)}
         completion(result)
     }
     
     func createProject(from template: TemplateModel, projectName: String, to url: URL, completion: @escaping ContextClosure<Error?>) {
         do {
-            let fileManager = FileManager.default
             let templateName = template.name
             let templateFileExtension = template.fileExtension
             let templateFileName = template.fileName
             let saveUrl = url.appendingPathComponent(projectName, isDirectory: true)
 
-            guard !fileManager.fileExists(atPath: saveUrl.path) else {
+            guard !self.fileManager.fileExists(atPath: saveUrl.path) else {
                 completion(TemplateServiceError.projectAlreadyExists)
                 return
             }
@@ -93,27 +108,38 @@ class TemplateService {
                 return
             }
 
-            try fileManager.clearDocumentDirectory()
+            try self.fileManager.clearDocumentDirectory()
 
             let documentDirectoryUrl = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
             let templateDocumentsZipUrl = documentDirectoryUrl.appendingPathComponent(templateFileName)
             let templateDocumentsUnzipUrl = documentDirectoryUrl
             let templateDocumentsFolderUrl = documentDirectoryUrl.appendingPathComponent(templateName)
 
-            try fileManager.copyItem(at: templateUrl, to: templateDocumentsZipUrl)
-            try fileManager.unzipItem(at: templateDocumentsZipUrl, to: templateDocumentsUnzipUrl)
-            try fileManager.removeIfExists(at: templateDocumentsZipUrl)
+            try self.fileManager.copyItem(at: templateUrl, to: templateDocumentsZipUrl)
+            try self.fileManager.unzipItem(at: templateDocumentsZipUrl, to: templateDocumentsUnzipUrl)
+            try self.fileManager.removeIfExists(at: templateDocumentsZipUrl)
 
             let renamer = XcodeProjectRenamer(oldName: templateName, newName: projectName)
             renamer.run(projectPath: templateDocumentsFolderUrl.path)
 
-            try fileManager.moveItem(at: templateDocumentsFolderUrl, to: saveUrl)
-            try fileManager.clearDocumentDirectory()
+            try self.fileManager.moveItem(at: templateDocumentsFolderUrl, to: saveUrl)
+            try self.fileManager.clearDocumentDirectory()
             
             completion(nil)
         } catch {
             completion(error)
         }
+    }
+    
+}
+
+// MARK: - Private
+private extension TemplateService {
+    
+    func getOrCreateTemplatesFolderURL() throws -> URL {
+        let url = try self.fileManager.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let templatesUrl = url.appendingPathComponent("Templates")
+        return templatesUrl
     }
     
 }
